323021d4f9e7856d46c55fdd7d52c57b7b718908
ezluci <ezluci1@gmail.com>
2022-04-28 15:45:02 +0300

    Added problems difficulty. Added script title. Clicking a problem opens a new tab.

--- pbinfo-get-unsolved.js
+++ pbinfo-get-unsolved.js
@@ -7,23 +7,17 @@ console.clear()
 
 
 // link prompt
-let pageLink = prompt('Pune un link catre categoria de probleme de unde vrei sa obtii problemele nerezolvate.\nMergi la o clasa, alege o categorie, si copiaza link-ul aici.\n\nExemple:\n\nhttps://www.pbinfo.ro/probleme/categorii/5/algoritmi-elementari-cifrele-unui-numar\n\nhttps://www.pbinfo.ro/?pagina=probleme-lista&disciplina=0&clasa=-1&dificultate=0&folosesc_consola=-1&eticheta=52')
+let pageLink = prompt('Pune un link către categoria de probleme de unde vrei să obții problemele nerezolvate.\nMergi la o clasă, alege o categorie, și copiază link-ul aici.\n\nExemple:\n\nhttps://www.pbinfo.ro/probleme/categorii/5/algoritmi-elementari-cifrele-unui-numar\n\nhttps://www.pbinfo.ro/?pagina=probleme-lista&disciplina=0&clasa=-1&dificultate=0&folosesc_consola=-1&eticheta=52')
 pageLink = pageLink.replaceAll(/(\?|&)start\d+/g, '')
 
 
 // clearing site
 document.head.innerHTML = ''
 document.body.innerHTML = ''
-
-
-// css
-const style = document.createElement('style')
-style.innerText = `
-   .logMessage {
-      display: block;
-   }
-`
-document.head.appendChild(style)
+const title = document.createElement('h2')
+title.style.display = 'block'
+title.innerHTML = '<span style="color: red"> pbinfo-get-unsolved.js</span>. <a href="https://github.com/ezluci/pbinfo-get-unsolved" target="_blank"><i>GitHub</i></a>'
+document.body.appendChild(title)
 
 
 // logging
@@ -36,8 +30,8 @@ function addLog(msg) {
    const date = new Date()
    const msgEl = document.createElement('span')
    msgEl.innerHTML = '<b>[' + date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0') + ':' + date.getSeconds().toString().padStart(2, '0') + '] - </b>' + msg
-   msgEl.classList += 'logMessage'
-
+   msgEl.style.display = 'block'
+ 
    logDiv.appendChild(msgEl)
    window.scroll(0, logDiv.scrollHeight)
 }
@@ -47,7 +41,7 @@ function addLog(msg) {
 const reqPageEl = document.createElement('div')
 reqPageEl.style.display = 'none'
 document.body.appendChild(reqPageEl)
-addLog(`Link catre categoria de probleme: <a href="${pageLink}"><i>${pageLink}</i></a>`)
+addLog(`Link către categoria de probleme: <a href="${pageLink}"><i>${pageLink}</i></a>`)
 
 const problems = []
 
@@ -55,7 +49,7 @@ const problems = []
 function logAllProblems() {
    addLog(`<u>Am terminat de extras problemele.</u> Sunt ${problems.length} probleme nerezolvate:`)
    problems.forEach(problem => {
-      addLog(`<a href="${problem.link}">#${problem.id} - ${problem.name}</a> (${problem.score}p)`)
+      addLog(`<a href="${problem.link}" target="_blank">#${problem.id} - ${problem.name}</a> (${problem.score}p) <span style="color: white; background-color:#${problem.difficulty === 'ușoară' ? '5cb85c' : problem.difficulty === 'medie' ? 'f0ad4e' : problem.difficulty === 'dificilă' ? '5bc0de' : 'd9534f'}">${problem.difficulty}</span>`)
    })
 }
 
@@ -78,6 +72,11 @@ function logAllProblems() {
          const id = pageProblems[i].children[0].children[0].children[0].innerText.trim().slice(1)
          const name = pageProblems[i].children[0].children[0].children[1].innerText.trim()
          const link = pageProblems[i].children[0].children[0].children[1].href.trim()
+         const difficulty = 
+               (pageProblems[i].getElementsByClassName('list-unstyled list-inline')[0].innerText.includes('ușoară') ? 'ușoară' :
+               pageProblems[i].getElementsByClassName('list-unstyled list-inline')[0].innerText.includes('medie') ? 'medie' :
+               pageProblems[i].getElementsByClassName('list-unstyled list-inline')[0].innerText.includes('dificilă') ? 'dificilă' :
+               'concurs')
          let score = pageProblems[i].children[2].children[1].children[0].innerText.trim().slice(7).trim()
 
          if (score === '100') {
@@ -85,7 +84,7 @@ function logAllProblems() {
          } else {
             if (score === '')
                score = '0'
-            problems.push({id: id, name: name, link: link, score: score})
+            problems.push({id: id, name: name, link: link, difficulty: difficulty, score: score})
          }
       }
       
